d0ac632e9b8b87c2b309583b1d4c4b576c914186
Aru <aru.yelemes23@gmail.com>
2020-09-26 00:10:47 +0600

    shop-inventory service

---
+++ src/main/java/kz/iitu/shop_inventory/repository/InventoryItemRepository.java
@@ -0,0 +1,11 @@
+package kz.iitu.shop_inventory.repository;
+
+import kz.iitu.shop_inventory.models.InventoryItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
+
+    Optional<InventoryItem> findByProductCode(String productCode);
+}
