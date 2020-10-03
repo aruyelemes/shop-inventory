5307476b9a173dad358831cd5a63b3234aa1a60b
Aru <aru.yelemes23@gmail.com>
2020-10-03 17:04:44 +0600

    Second commit

--- src/main/java/kz/iitu/shop_inventory/ShopInventoryApplication.java
+++ src/main/java/kz/iitu/shop_inventory/ShopInventoryApplication.java
@@ -2,8 +2,10 @@ package kz.iitu.shop_inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class ShopInventoryApplication {
 
 	public static void main(String[] args) {
